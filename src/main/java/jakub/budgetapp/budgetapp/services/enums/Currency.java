7c354542348d1013c1ce8cce72ccdf9f59e00b3b
jbialy <bialyj1@gmail.com>
2019-08-11 23:30:33 +0200

    enum Currency, CsvReaderHelperImpl changed into BankFactory, interface removed

---
+++ src/main/java/jakub/budgetapp/budgetapp/services/enums/Currency.java
@@ -0,0 +1,5 @@
+package jakub.budgetapp.budgetapp.services.enums;
+
+public enum Currency {
+    PLN, UNKNOWN
+}
--- src/main/java/jakub/budgetapp/budgetapp/services/implementations/BankFactory.java
+++ src/main/java/jakub/budgetapp/budgetapp/services/implementations/BankFactory.java
@@ -1,6 +1,5 @@
 package jakub.budgetapp.budgetapp.services.implementations;
 
-import jakub.budgetapp.budgetapp.services.CsvReaderHelper;
 import jakub.budgetapp.budgetapp.services.enums.Bank;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,7 +10,7 @@ import java.io.InputStreamReader;
 import java.util.Optional;
 
 @Service
-public class CsvReadHelperImpl implements CsvReaderHelper {
+public class BankFactory {
     // this guys below should be put into some kind of properties!
     // duplicates!
     private static final String COMMA_SEPARATOR = ";";
@@ -19,8 +18,7 @@ public class CsvReadHelperImpl implements CsvReaderHelper {
     private static final String KEY_WORD_MBANK = "mBank S.A. Bankowość Detaliczna";
     private static final String KEY_WORD_EUROBANK = "Numer konta:39 1470 0002 2007 2197 1000 0001";
 
-    @Override
-    public Optional<Bank> checkBankType (MultipartFile csvFile) {
+    Optional<Bank> getBank(MultipartFile csvFile) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(csvFile.getInputStream(), CHARSET_NAME)) ) {
 
             String line;
